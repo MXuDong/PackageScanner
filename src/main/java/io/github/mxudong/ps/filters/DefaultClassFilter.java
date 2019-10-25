f24e34bf4bc4cd126261c6e93cb34b577ab53da1
MXuDong <2941884109@qq.com>
2019-10-25 23:02:42 +0800

    rename class : DefaultClassFilter.java

---
+++ src/main/java/io/github/mxudong/ps/filters/DefaultClassFilter.java
@@ -0,0 +1,25 @@
+package io.github.mxudong.ps.filters;
+
+import java.io.File;
+
+/**
+ * The DefaultClassFilter implements FileScannerFilterInterface.
+ *
+ * The DefaultClassFilter only pass the file end with {@code .class}.
+ * And if the file is directory, it also can be passed. And if file
+ * is null, it will return false.
+ *
+ * @author Dong
+ * @since 1.0.0
+ * @see FileScannerFilterInterface
+ */
+
+public class DefaultClassFilter implements FileScannerFilterInterface {
+    public boolean canPass(File file) {
+        if(file == null){
+            return false;
+        }
+
+        return file.isDirectory() || file.getName().endsWith(".class");
+    }
+}
