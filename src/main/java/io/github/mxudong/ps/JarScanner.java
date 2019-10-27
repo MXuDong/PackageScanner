67ed1b586dcbf618b66513c8336307ff0d7287fb
MXuDong <2941884109@qq.com>
2019-10-27 22:47:27 +0800

    add JarScanner

---
+++ src/main/java/io/github/mxudong/ps/JarScanner.java
@@ -0,0 +1,85 @@
+package io.github.mxudong.ps;
+
+import io.github.mxudong.ps.filters.JarScannerFilterInterface;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * The jar scanner provide method for JAR scanner operation.
+ *
+ * @author Dong
+ * @since 1.0.0
+ */
+
+public class JarScanner {
+    /**
+     * the jar file's end string
+     */
+    public static final String JAR_FILE_SUFFIX = ".jar";
+
+
+    private JarFile targetJarFile;
+
+    /**
+     * The default construction, if the jarFile is null, the
+     * targetJarFile will be set null.
+     *
+     * @param jarFile target jar file
+     */
+    public JarScanner(JarFile jarFile) {
+        if (jarFile == null) {
+            this.targetJarFile = null;
+        } else {
+            this.targetJarFile = jarFile;
+        }
+    }
+
+
+    /**
+     * The construction, if the target path is not exists, the
+     * jar file will be set null.
+     *
+     * @param jarPath target file path
+     */
+    public JarScanner(String jarPath) {
+        if (jarPath.endsWith(JarScanner.JAR_FILE_SUFFIX)) {
+            if (new File(jarPath).exists()) {
+                try {
+                    this.targetJarFile = new JarFile(jarPath);
+                } catch (IOException e) {
+                    this.targetJarFile = null;
+                }
+            } else {
+                this.targetJarFile = null;
+            }
+        }
+    }
+
+    /**
+     * scan the target jar file from the construction. The filter
+     * will block all types of files, such as directories and files.
+     *
+     * @param filter the filter to block files.
+     * @return the jar file
+     */
+    public List<JarEntry> doScan(JarScannerFilterInterface filter) {
+        List<JarEntry> r = new ArrayList<>();
+        Enumeration<JarEntry> enumeration = this.targetJarFile.entries();
+
+        while (enumeration.hasMoreElements()) {
+            JarEntry jarEntry = enumeration.nextElement();
+            if (filter.canAccess(jarEntry)) {
+                r.add(jarEntry);
+            }
+        }
+
+        return r;
+    }
+
+}
