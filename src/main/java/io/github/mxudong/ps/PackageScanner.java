1c215ca358579789b72f96b87d5086dcace6633a
MXuDong <2941884109@qq.com>
2019-10-28 23:09:45 +0800

    add class for PC main

---
+++ src/main/java/io/github/mxudong/ps/PackageScanner.java
@@ -0,0 +1,14 @@
+package io.github.mxudong.ps;
+
+/**
+ * This class provide some method for package scan in JAVA. Use this
+ * class, the program can quick get target class which want.
+ *
+ * @author Dong
+ * @since 1.0.0
+ */
+
+public class PackageScanner {
+
+
+}
