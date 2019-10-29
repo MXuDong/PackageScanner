ffa9a5f3af66cae4595f7553462cdb10ec634443
MXuDong <2941884109@qq.com>
2019-10-29 20:04:39 +0800

    add class : ClassCaches

---
+++ src/main/java/io/github/mxudong/ps/caches/ClassCaches.java
@@ -0,0 +1,12 @@
+package io.github.mxudong.ps.caches;
+
+/**
+ * For class cache, be cache type must be class. It will create by
+ * cache manager, so in this class, is not singleton pattern.
+ *
+ * @author Dong
+ * @since 1.0.0
+ */
+
+public class ClassCaches {
+}
